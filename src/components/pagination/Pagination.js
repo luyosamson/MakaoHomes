4a8e55372830b59b0b4c350a5700a458a4872f5f
luyosamson <luyosamson@gmail.com>
2023-02-25 12:34:16 +0300

    Created pagination

---
+++ src/components/pagination/Pagination.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Pagination.css'
+
+function Pagination({totalPosts,postPerPage,setCurrentPage,currentPage}) {
+
+    let pages=[]
+
+    for(let i=1;i<=Math.ceil(totalPosts/postPerPage);i++){
+        pages.push(i)
+    }
+
+  return (
+    <div className='pagination'>
+        
+        {
+            pages.map((page,index)=>{
+                return <button key={index}
+                onClick={()=>setCurrentPage(page)}
+                className={page === currentPage?"active":""}
+                >
+                    {page}</button>
+            })
+        }
+
+    </div>
+  )
+}
+
+export default Pagination
+
+
